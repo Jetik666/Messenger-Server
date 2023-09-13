fb5b9c809bd5ca1e7377b49b3c70fb38f0dfbff1
Jetik666 <denis13po@gmail.com>
2023-09-13 20:51:28 +0700

    Menu bar animation

--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 using Core;
 
 namespace Server
@@ -8,21 +13,24 @@ namespace Server
     {
         private readonly NetworkServerHost _host;
 
-
         public MainWindow()
         {
+            // Turn off hardware acceleration
+            // Hide geforce experience overlay
+            RenderOptions.ProcessRenderMode = System.Windows.Interop.RenderMode.SoftwareOnly;
+
             InitializeComponent();
 
             _host = new NetworkServerHost();
             //_ = host.Start();
         }
 
-        private void MinimizeProgram(object sender, EventArgs e)
+        private void MinimizeProgram(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
         }
 
-        private void MaximizeProgram(object sender, EventArgs e)
+        private void MaximizeProgram(object sender, RoutedEventArgs e)
         {
             switch (WindowState)
             {
@@ -39,10 +47,57 @@ namespace Server
             }
         }
 
-        private void CloseProgram(object sender, EventArgs e)
+        private void CloseProgram(object sender, RoutedEventArgs e)
         {
             _ = _host.Close();
             Close();
+        }
+
+        private async void ShowDesc(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                if (Math.Abs(MenuBarWidth.Width.Value - 128) < 0.001)
+                {
+                    await ShowAllAnimation(128, 32);
+                    button.Content = "|->";
+                    Debug.WriteLine($"Hide {Math.Abs(MenuBarWidth.Width.Value - 128)}");
+                }
+                else
+                {
+                    await ShowAllAnimation(32, 128);
+                    button.Content = "<-|";
+                    Debug.WriteLine($"All {Math.Abs(MenuBarWidth.Width.Value - 128)}");
+                }
+            }
+            else
+            {
+                Debug.WriteLine("ShowDesc function was called by non-Button sender.");
+            }
+        }
+
+        private async Task ShowAllAnimation(double fromWidth, double toWidth)
+        {
+            MenuBarWidth.Width = new GridLength(toWidth);
+
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                From = fromWidth,
+                To = toWidth,
+                Duration = TimeSpan.FromMilliseconds(100)
+            };
+
+            // Check that the MenuBarWidth is correctly bound and the WidthProperty is the correct dependency property
+            if (MenuBarWidth != null && WidthProperty != null)
+            {
+                MenuBarWidth.BeginAnimation(WidthProperty, animation);
+                await Task.Delay(100); // Add a small delay to ensure the animation has time to start
+                Debug.WriteLine("Show all animation is finished.");
+            }
+            else
+            {
+                Debug.WriteLine("MenuBarWidth or WidthProperty is null.");
+            }
         }
     }
 }

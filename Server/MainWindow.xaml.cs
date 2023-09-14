ac98720a9472e131d6829023ecb7d3f8904fac55
Jetik666 <denis13po@gmail.com>
2023-09-14 17:19:48 +0700

    few fixes

--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
+
 using Core;
 
 namespace Server
